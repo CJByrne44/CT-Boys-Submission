e58216a317cf90e7e04c71ca0c8f1baef4145787
CJByrne44 <70672489+CJByrne44@users.noreply.github.com>
2020-11-20 21:18:05 -0500

    Database stuff

---
+++ connnect_database.py
@@ -0,0 +1,28 @@
+from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
+
+import sys
+import csv
+
+cloud_config= {
+        'secure_connect_bundle': 'C:\\Users\conne\\PycharmProjects\\kivyProject\\secure-connect-users.zip'
+}
+auth_provider = PlainTextAuthProvider('CTBoys', 'CTBoyspassword1')
+cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
+session = cluster.connect()
+
+session.execute("USE users")
+
+with open('people.csv', 'r') as csvFile:
+        reader = csv.DictReader(csvFile)
+        for line in reader:
+                test = session.execute("SELECT id FROM users")
+                print(test)
+                # session.execute("INSERT INTO users (id, first, last, university, major) VALUES(?, ?, ?, ?, ?)",
+                #            (line['first'], line['last'], line['university'],
+                #             line['major']))
+
+
+rows = session.execute("SELECT * FROM users")
+for row in rows:
+        print(row)
