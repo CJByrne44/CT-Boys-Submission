2d1c17ad88e6ce805d042f04524bde1a22fbafc3
CJByrne44 <70672489+CJByrne44@users.noreply.github.com>
2020-11-21 15:25:54 -0500

    fixing things

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
-from database_functions import initDatabase
 
-session = initDatabase()
 class MyGrid(GridLayout):
     def __init__(self, **kwargs):
         super(MyGrid, self).__init__(**kwargs)
