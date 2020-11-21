216e7c8112b34d8c2bca0107e020b6243b1c41b3
ujalil101 <74789609+ujalil101@users.noreply.github.com>
2020-11-20 19:58:34 -0500

    insert commit message

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 
-
 class MyGrid(GridLayout):
     def __init__(self, **kwargs):
         super(MyGrid, self).__init__(**kwargs)
@@ -22,13 +21,29 @@ class MyGrid(GridLayout):
         self.lastName = TextInput(multiline=False)
         self.inside.add_widget(self.lastName)
 
+        self.inside.add_widget(Label(text="Gender: "))
+        self.gender = TextInput(multiline=False)
+        self.inside.add_widget(self.gender)
+
         self.inside.add_widget(Label(text="Email: "))
         self.email = TextInput(multiline=False)
         self.inside.add_widget(self.email)
 
+        self.inside.add_widget(Label(text="University: "))
+        self.university = TextInput(multiline=False)
+        self.inside.add_widget(self.university)
+
+        self.inside.add_widget(Label(text="Major "))
+        self.major = TextInput(multiline=False)
+        self.inside.add_widget(self.major)
+
+
+
+
+
         self.add_widget(self.inside)
 
-        self.submit = Button(text="Submit", font_size=40)
+        self.submit = Button(text="Submit", font_size=25)
         self.submit.bind(on_press=self.pressed)
         self.add_widget(self.submit)
 
@@ -36,16 +51,33 @@ class MyGrid(GridLayout):
         name = self.name.text
         last = self.lastName.text
         email = self.email.text
-# Comment test
-        print("Name:", name, "Last Name:", last, "Email:", email)
+        gender = self.gender.text
+        univ = self.university.text
+        major = self.major.text
+        # Comment test
+        print("Name:\t\t\t\t\t |", name)
+        print("Last Name:\t\t\t\t |", last)
+        print("Gender\t\t\t\t\t |", gender)
+        print("Major: \t\t\t\t\t |", major)
+        print("University:\t\t\t\t |",univ)
+        print("Email:\t\t\t\t\t |", email)
         self.name.text = ""
         self.lastName.text = ""
+        self.gender.text = ""
+        self.major.text = ""
         self.email.text = ""
+        ##self.univ.text = ""
 
-class MyApp(App):
+
+
+
+
+
+class TestApp(App):
     def build(self):
+
         return MyGrid()
 
 
-if __name__ == "__main__":
-    MyApp().run()
+if __name__ == '__main__':
+    TestApp().run()
